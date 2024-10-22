7fbd44826b9102310fa8bb545f4fa202e3b1c5dd
AndersonDev2022 <canalfiquesabendo2019top@gmail.com>
2024-10-22 18:21:09 -0300

    Commit 2

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 
 import app from './app';
 
-app.listen(3001,function(){
-    console.log('🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢');
-    console.log('🟢🟢🟢🟢    SERVER IS RUNNING ON PORT 3001    🟢🟢🟢🟢');
-    console.log('🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢');
-})
+const port = process.env.PORT || 3001;
+
+app.listen(port,'0.0.0.0')
+
+// app.listen(3001,function(){
+//     console.log('🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢');
+//     console.log('🟢🟢🟢🟢    SERVER IS RUNNING ON PORT 3001    🟢🟢🟢🟢');
+//     console.log('🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢');
+// })
